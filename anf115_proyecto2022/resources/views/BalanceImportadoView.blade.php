9c2147f59f059b075b382d143bf1afd88cc94ca9
William Garcia <stanley.19.10@outlook.com>
2022-11-09 02:26:53 -0600

    integrando plantilla

--- anf115_proyecto2022/resources/views/BalanceImportadoView.blade.php
+++ anf115_proyecto2022/resources/views/BalanceImportadoView.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-<h1>{{$nomE}}</h1>
-
 
 @section('menu')
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
@@ -10,42 +8,39 @@
       <img src="{{ asset('vendors/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">SisCont_ANF-2022</span>
     </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('vendors/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+            <img src="{{ asset('vendors/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Bienvenido Usuario</a>
+            <a href="#" class="d-block">Bienvenido Usuario</a>
         </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
+    </div>
+    
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <div>
-            <!-- <a href="{{route('empresa_insert')}}" class="btn btn-primary">Agregar Empresa</a> -->
-        </div>
+            <div>
+                <!-- <a href="{{route('empresa_insert')}}" class="btn btn-primary">Agregar Empresa</a> -->
+            </div>
         </ul>
-      </nav>  <!-- /.sidebar-menu -->
-    </div>  <!-- /.sidebar -->
+    </nav>  <!-- /.sidebar-menu -->
+</div>  <!-- /.sidebar -->
 </aside>
 
 @endsection
 
 @section('content')
+<script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</script>
 
-
-   
-    <h2>{{$nombreEstado}}</h2>
-   
-    <script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </script>
-    
+    <h1>{{$nomE}}</h1>  
+    <h3>{{$nombreEstado}}</h3>
+    <p>prueba</p>
 
 <?php
     $mensaje_error_si_no = '';
